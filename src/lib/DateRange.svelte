139f93bf6fc80a3db88e9c604cb4f7dc3502b24d
Jed Fonner <gitlab@fonner.dev>
2026-01-25 20:24:02 -0500

    Updates date range styling

--- src/lib/DateRange.svelte
+++ src/lib/DateRange.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import rightArrowIcon from '../assets/right-arrow.svg';
-
   let { startStr = $bindable(), endStr = $bindable() } = $props<{
     startStr: string;
     endStr: string;
@@ -15,14 +13,21 @@
   };
 </script>
 
-<div class="date-range flex">
+<div class="date-range">
+  <!-- top row -->
+  <div class="label">From</div>
+  <div></div>
+  <div class="label">To</div>
+  <div></div>
+  <!-- second row -->
+
   <div class="picker">
     <input type="date" bind:value={start} />
   </div>
-  <div class="divider">
-    <img src={rightArrowIcon} alt="to" />
+  <div class="divider">➙</div>
+  <div class="picker">
+    <input type="date" bind:value={end} />
   </div>
-  <div class="picker"><input type="date" bind:value={end} /></div>
   <div>
     <button onclick={submit}> Update RTO </button>
   </div>
@@ -31,20 +36,28 @@
 <style>
   .date-range {
     --font-size: 1.5rem;
+    display: grid;
+    grid-template-columns: 1fr auto 1fr auto;
   }
   .picker {
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    align-items: flex-start;
     gap: 0.5rem;
+  }
+  .label {
+    font-size: 0.9rem;
   }
   .picker input[type='date'] {
     font-family: system-ui, Avenir, Helvetica, Arial, sans-serif;
     font-size: var(--font-size);
     padding: 0.25rem;
+    color: var(--foreground-color);
   }
-  .divider img {
-    width: 2rem;
-    height: 2rem;
+
+  .divider {
+    font-size: 2rem;
+    margin: 0 1rem;
   }
   button {
     font-size: var(--font-size);
@@ -53,8 +66,8 @@
     color: var(--foreground-color);
     border: 1px solid var(--foreground-color);
     border-radius: 0.25rem;
-    height: 100%;
     min-width: max-content;
+    margin-left: 1rem;
   }
   button:hover {
     transform: scale(1.05);
